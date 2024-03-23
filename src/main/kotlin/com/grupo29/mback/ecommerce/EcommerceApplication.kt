5881f561f32988c1fcb1cc690d31b114277fa70a
Marcelo <marcelo.leite@mercadolivre.com>
2024-03-23 11:50:38 -0300

    config inicial

---
+++ src/main/kotlin/com/grupo29/mback/ecommerce/EcommerceApplication.kt
@@ -0,0 +1,11 @@
+package com.grupo29.mback.ecommerce
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EcommerceApplication
+
+fun main(args: Array<String>) {
+    runApplication<EcommerceApplication>(*args)
+}
