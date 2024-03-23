2b861ee66fd37d2471f0fe3ab2742a51d5151ff9
Marcelo <marcelo.leite@mercadolivre.com>
2024-03-23 16:24:10 -0300

    config test

--- app/src/main/kotlin/com/grupo29/mback/ecommerce/EcommerceApplication.kt
+++ app/src/main/kotlin/com/grupo29/mback/ecommerce/EcommerceApplication.kt
@@ -1,11 +1,17 @@
 package com.grupo29.mback.ecommerce
 
+import com.grupo29.mback.ecommerce.application.config.setupEnv
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableR2dbcAuditing
 class EcommerceApplication
 
 fun main(args: Array<String>) {
+    setupEnv()
     runApplication<EcommerceApplication>(*args)
 }
